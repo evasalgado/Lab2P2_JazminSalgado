f4f535cf5af14ac89eaa8d38bc36fcc1410b606d
evasalgado <imviarsha@gmail.com>
2023-10-20 14:20:59 -0600

    menu base agregado a main

--- Lab2P2_JazminSalgado/src/lab2p2_jazminsalgado/Lab2P2_JazminSalgado.java
+++ Lab2P2_JazminSalgado/src/lab2p2_jazminsalgado/Lab2P2_JazminSalgado.java
@@ -4,6 +4,8 @@
  */
 package lab2p2_jazminsalgado;
 
+import java.util.Scanner;
+
 /**
  *
  * @author evaja
@@ -14,11 +16,30 @@ public class Lab2P2_JazminSalgado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int op=0;
-        while (op!=4) {            
+        Scanner leer = new Scanner(System.in);
+        int op = 0;
+        while (op != 4) {
             System.out.println("---MENU----\n"
-                    + "");
+                    + "1. Opción de Jugadores\n"
+                    + "2. Opción de Carros\n"
+                    + "3. Compra y venta de carros\n"
+                    + "4. Salir");
+            op = leer.nextInt();
+            switch (op) {
+                case 1:
+                    break;
+                case 2: 
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("GRACIAS POR UTILIZAR MI PROGRAMA");
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion valida");
+                    break;
+            }
         }
     }
-    
+
 }
