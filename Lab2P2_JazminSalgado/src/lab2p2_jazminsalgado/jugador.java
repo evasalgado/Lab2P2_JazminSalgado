da5a491116ce9df0da7734c54d9d8dbb63b60532
evasalgado <imviarsha@gmail.com>
2023-10-20 13:50:03 -0600

    Clase de jugador: constructores y mutadores añadidos

--- Lab2P2_JazminSalgado/src/lab2p2_jazminsalgado/jugador.java
+++ Lab2P2_JazminSalgado/src/lab2p2_jazminsalgado/jugador.java
@@ -13,12 +13,58 @@ import java.util.ArrayList;
 public class jugador {
     private String nombre;
     private int cantcarro;
-    private ArrayList carros = new ArrayList();
+    private ArrayList<carro> carros = new ArrayList();
     private int puntosrep;
     private double cuenta;
     
     public jugador(){
         
     }
-//    public jugador 
+    public jugador(String n){
+        nombre=n;
+    } 
+    public jugador(String n, int cc){
+        nombre=n;
+        cantcarro=cc;
+    }
+    
+    public void setNombre(String n){
+        nombre=n;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setCantcarro(int cc){
+        if (cc>0) {
+            cantcarro=cc;
+        } 
+    } 
+    public int getCantcarri(){
+        return cantcarro;
+    }
+    public void setCarros(ArrayList<carro> c){
+        carros=c;
+    }
+    public ArrayList<carro> getCarros(){
+        return carros;
+    }
+    public void setPuntosrep(int pr){
+        puntosrep=pr;
+    }
+    public int getPuntosreo(){
+        return puntosrep;
+    }
+    public void setCuenta(double c){
+        cuenta=c;
+    } 
+    public double getCuenta(){
+        return cuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Datos de Jugador: "+nombre+ "\n carros: "+carros+"\npuntos de reputación: "+puntosrep+"\nDinero actual: "+cuenta;
+    }
+    
+    
 }
