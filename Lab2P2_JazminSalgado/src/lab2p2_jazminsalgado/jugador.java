d10816c018bd43941bb95ffe20663306934913dd
evasalgado <imviarsha@gmail.com>
2023-10-20 13:31:14 -0600

    Clases carro y jugador creadas

---
+++ Lab2P2_JazminSalgado/src/lab2p2_jazminsalgado/jugador.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2p2_jazminsalgado;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author evaja
+ */
+public class jugador {
+    private String nombre;
+    private int cantcarro;
+    private ArrayList carros = new ArrayList();
+    private int puntosrep;
+    private double cuenta;
+    
+    public jugador(){
+        
+    }
+//    public jugador 
+}
